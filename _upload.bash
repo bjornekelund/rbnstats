23724db2a50d86e91b0b4b750cdeb9c0e82861a4
Björn <bjorn@ekelund.nu>
2021-10-20 20:02:55 +0000

    Update

--- _upload.bash
+++ _upload.bash
@@ -4,11 +4,8 @@ ftp -n ftp.server.com  <<END_SCRIPT
 quote USER UsErNaMe
 quote PASS pAsSwOrD
 cd sm7iun.se/targetfolder
-put rbnskew.txt
-put rbnskew2.txt
-put rbnhist.txt
-put rbnact.txt
-put anchors.txt
+put rbnstats.txt
+put rbnstatsp.txt
 quit
 END_SCRIPT
 exit 0
